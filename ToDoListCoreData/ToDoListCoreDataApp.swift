bc858e7545afaa864424813dea53c900c8d2d1b2
Tom Huynh <tomhuynhsg@gmail.com>
2023-09-09 20:12:50 +0700

    Initial Commit

---
+++ ToDoListCoreData/ToDoListCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListCoreDataApp.swift
+//  ToDoListCoreData
+//
+//  Created by Tom Huynh on 09/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
